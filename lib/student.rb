2028151f4a64ced2ce28c1e26924ceac022f5de7
Brandon Green <brandon@brandonleeproductions.com>
2018-08-15 00:31:18 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -47,6 +47,9 @@ class Student
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
-
+  def update
+    sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
+  end
 
 end
